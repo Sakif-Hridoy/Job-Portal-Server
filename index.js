b4de78227eba9458ecc393b7f35bb14c71cb6436
Sakif <sakifworkspace@gmail.com>
2025-03-28 22:23:53 +0600

    secure some routes,apis with jwt and x-api-key

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
+    allowedHeaders: ["x-api-key", "Content-Type", "Authorization"], // Ensure x-api-key is allowed
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
+
+// jwt verify middleware
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
   console.log("verify token", token);
@@ -33,6 +37,26 @@ const verifyToken = (req, res, next) => {
   next();
 };
 
+
+// x-api-key verify middleware
+const verifyApiKey = (req, res, next) => {
+  const apiKey = req?.headers?.["x-api-key"];
+  console.log(apiKey);
+  // console.log(req.headers);
+
+  if (!apiKey) {
+    return res.status(401).json({ message: "API Key Missing" });
+  }
+
+  if (apiKey !== process.env.API_KEY) {
+    return res.status(403).json({ message: "Invalid API Key" });
+  }
+
+  next();
+};
+
+
+// mongodb connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.djg6r.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,14 +73,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // database and collections
     const jobsCollection = client.db("job-portal").collection("jobs");
     const jobApplicationsCollection = client
       .db("job-portal")
       .collection("job-applications");
 
-    app.post("/jwt", async (req, res) => {
+    // signin/login verification with jwt and x-api-key
+    app.post("/jwt",verifyApiKey, async (req, res) => {
       const user = req.body;
-      const token = await jwt.sign(user, process.env.JWT_SECRET, {
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
         expiresIn: "5h",
       });
       console.log("first created", token);
@@ -68,6 +94,7 @@ async function run() {
         .send({ success: true });
     });
 
+    // clear cookie when logout
     app.post("/logout", (req, res) => {
       res
         .clearCookie("token", {
@@ -77,8 +104,9 @@ async function run() {
         .send({ success: true });
     });
 
-    app.get("/jobs", async (req, res) => {
-      const email = req.query.email;
+    //all jobs route
+    app.get("/jobs",verifyApiKey, async (req, res) => {
+      const email = req?.query?.email;
       let query = {};
       if (email) {
         query = { hr_email: email };
@@ -88,21 +116,24 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/jobs/:id", async (req, res) => {
-      const id = req.params.id;
+    //get a single job 
+    app.get("/jobs/:id",async (req, res) => {
+      const id = req?.params?.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
     });
 
+    // get all job applications by token and user email verified
+    // join another mongodb collection and show mutual collections data
     app.get("/job-applications", verifyToken, async (req, res) => {
-      const email = req.query.email;
+      const email = req?.query?.email;
       const query = { applicant_email: email };
-      if (req.user?.email !== email) {
+      if (req?.user?.email !== email) {
         res.status(403).send({ message: "forbidden access" });
       }
       const result = await jobApplicationsCollection.find(query).toArray();
-      console.log("cookies", req.cookies);
+      // console.log("cookies", req.cookies);
       for (const application of result) {
         // console.log(application.job_id)
         const query1 = { _id: new ObjectId(application.job_id) };
@@ -118,8 +149,10 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/job-application", async (req, res) => {
-      const newApplication = req.body;
+    // post a job application
+    //count a users job application by joining two collections
+    app.post("/job-application",verifyToken,verifyApiKey,async (req, res) => {
+      const newApplication = req?.body;
       const result = await jobApplicationsCollection.insertOne(newApplication);
 
       const id = newApplication.job_id;
@@ -143,23 +176,26 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/jobs", async (req, res) => {
-      const newJob = req.body;
+    // post a job/ by recruiter
+    app.post("/job",verifyApiKey,verifyToken, async (req, res) => {
+      const newJob = req?.body;
       const result = await jobsCollection.insertOne(newJob);
       res.send(result);
       // console.log(result);
     });
 
+    // get job applications by user job_id
     app.get("/job-applications/jobs/:job_id", async (req, res) => {
-      const jobId = req.params.job_id;
+      const jobId = req?.params?.job_id;
       // console.log(jobId);
       const query = { job_id: jobId };
       const result = await jobApplicationsCollection.find(query).toArray();
       res.send(result);
     });
 
+    // update a job application
     app.patch("/job-applications/:id", async (req, res) => {
-      const id = req.params.id;
+      const id = req?.params?.id;
       const data = req.body;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
