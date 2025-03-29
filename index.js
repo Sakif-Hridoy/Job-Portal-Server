b000c205a29f9d1a20da1cbe3a64e008c74cfcf1
Sakif <sakifworkspace@gmail.com>
2025-03-29 12:06:21 +0600

    fix my-applications

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ async function run() {
     });
 
     //all jobs route
-    app.get("/jobs",verifyToken,verifyApiKey, async (req, res) => {
+    app.get("/jobs",verifyToken, async (req, res) => {
       const email = req?.query?.email;
       let query = {};
       if (email) {
